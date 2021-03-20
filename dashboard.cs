801c80ce4756280618abae9ebf540ea778034ca5
RositsaRuseva <rossyplruseva@gmail.com>
2021-03-20 16:08:25 +0200

    Login and Register project

---
+++ dashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_And_Register_Form
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
